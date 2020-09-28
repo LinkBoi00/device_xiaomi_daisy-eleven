256936867f4d5df21b81ac078169bbaa238c8028
Dyrex2004 <dyrex2004@gmail.com>
2020-09-28 18:29:55 +0200

    daisy: downrev Soundtrigger HAL to 2.2

--- device.mk
+++ device.mk
@@ -93,7 +93,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.soundtrigger@2.3-impl \
+    android.hardware.soundtrigger@2.2-impl \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.r_submix.default \
