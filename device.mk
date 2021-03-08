4472f92f840b053b4cf9e0c68c4f61704b892366
ItsVixano <vixanonew@gmail.com>
2021-03-08 20:18:17 +0100

    daisy: Switch to oss camera hal
    Change-Id: If62af39d99b8316a3ce551cd4d9fbeae63264716

--- device.mk
+++ device.mk
@@ -187,8 +187,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     android.hardware.camera.device@3.4 \
+    camera.msm8953 \
     vendor.qti.hardware.camera.device@1.0.vendor \
     libdng_sdk.vendor \
+    libmm-qcamera \
     libgui_vendor \
     Snap
 
