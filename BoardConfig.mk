b4727419bb34ac7a6e57f550e0e6e232a9c62ef6
opal06 <xy.lu11@gmail.com>
2021-03-08 11:51:58 +0000

    daisy: Adapt for use with msm8953-common vendor

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -226,3 +226,4 @@ TARGET_HAS_BROKEN_WLAN_SET_INTERFACE := true
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/daisy/BoardConfigVendor.mk
+-include vendor/xiaomi/msm8953-common/BoardConfigVendor.mk
