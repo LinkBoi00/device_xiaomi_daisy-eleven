66c5f2af7fbfcc12a426b00f2582e6424bf1d6ba
acras01 <acras1@gmail.com>
2021-06-13 16:11:34 +0300

    daisy: Add additional BT audio props (APTX, LDAC)

--- prop.mk
+++ prop.mk
@@ -42,6 +42,9 @@ vendor.audio.feature.kpi_optimize.enable=false
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
+persist.bt.enableAptXHD=true \
+persist.service.btui.use_aptx=1 \
+persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxhd-ldac \
 persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
 persist.bluetooth.a2dp_offload.disabled=false \
 persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptive \
